fc9434b9496d448fbcc51660e149c1996bfd5d98
mani-hash <manimehalan400@gmail.com>
2026-07-02 20:59:01 +0530

    chore(fix): move ignore list to top

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,6 @@
 import { tanstackConfig } from '@tanstack/eslint-config'
 
 export default [
-  ...tanstackConfig,
-  {
-    rules: {
-      'import/no-cycle': 'off',
-      'import/order': 'off',
-      'sort-imports': 'off',
-      '@typescript-eslint/array-type': 'off',
-      '@typescript-eslint/require-await': 'off',
-      'pnpm/json-enforce-catalog': 'off',
-    },
-  },
   {
     ignores: [
       '**/.output/**',
@@ -26,6 +15,17 @@ export default [
       'eslint.config.js',
       'prettier.config.js',
     ],
+  },
+  ...tanstackConfig,
+  {
+    rules: {
+      'import/no-cycle': 'off',
+      'import/order': 'off',
+      'sort-imports': 'off',
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/require-await': 'off',
+      'pnpm/json-enforce-catalog': 'off',
+    },
   },
   {
     files: ['src/**/*.test.{ts,tsx}'],
