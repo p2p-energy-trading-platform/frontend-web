1faa8a9528a5f09dd472fb2dee5932fd4567cd3f
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-26 04:30:16 +0530

    feat(landing): implemented hero section of landing page

---
+++ src/components/landing/HeroVisual.tsx
@@ -0,0 +1,16 @@
+export default function HeroVisual() {
+
+    return (
+
+        <div className="">
+
+                <img
+                    src="/images/gridx-energy-ecosystem.png"
+                    alt="GridX energy ecosystem connecting solar, battery, EV and smart homes"
+                />
+
+        </div>
+
+    );
+
+}
