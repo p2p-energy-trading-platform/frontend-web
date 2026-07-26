534f4afc3dd6d58cb00ed361ded9a33deeed7861
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-27 00:41:50 +0530

    style: format route file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,23 +3,18 @@ import HeroSection from '@/components/landing/HeroSection'
 import HowItWorksSection from '#/components/landing/HowItWorksSection'
 import FeaturesSection from '#/components/landing/FeaturesSection'
 
-
 export const Route = createFileRoute('/')({
-    component: Home,
+  component: Home,
 })
 
-
 function Home() {
-
   return (
     <main>
-
       <HeroSection />
 
       <HowItWorksSection />
 
       <FeaturesSection />
-
     </main>
   )
 }
