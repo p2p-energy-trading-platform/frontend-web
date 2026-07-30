f4e071abcf87d23049a099f48a0f641d02adcaa8
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-31 01:04:16 +0530

    refactor: update hero section import path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import HeroSection from '@/components/landing/HeroSection'
+import HeroSection from '#/components/landing/hero-section/HeroSection'
 import HowItWorksSection from '@/components/landing/HowItWorksSection'
 import BenifitsSection from '@/components/landing/benifits-section/BenefitsSection'
 import FAQSection from '@/components/landing/faq-section/FAQSection'
