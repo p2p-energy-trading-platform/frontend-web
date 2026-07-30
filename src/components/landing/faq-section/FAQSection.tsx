780b8f93e8390841c0f7e352564b5b4fa34350c2
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-31 01:57:30 +0530

    style: remove temporary border from faq section

--- src/components/landing/faq-section/FAQSection.tsx
+++ src/components/landing/faq-section/FAQSection.tsx
@@ -3,7 +3,7 @@ import FAQCard from './FAQCard'
 
 export default function FAQSection() {
   return (
-    <section className="border border-red-500 bg-bg-surface flex flex-col items-center justify-center">
+    <section className="bg-bg-surface flex flex-col items-center justify-center">
 
         <FAQHeader />
 
