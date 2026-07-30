519c6c050beff314896622819213a0fa44344395
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-31 00:56:23 +0530

    refactor: replace import alias # with @

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import HeroSection from '@/components/landing/HeroSection'
-import HowItWorksSection from '#/components/landing/HowItWorksSection'
-import BenifitsSection from '#/components/landing/benifits-section/BenefitsSection'
-import FAQSection from '@/components/landing/FAQSection'
+import HowItWorksSection from '@/components/landing/HowItWorksSection'
+import BenifitsSection from '@/components/landing/benifits-section/BenefitsSection'
+import FAQSection from '@/components/landing/faq-section/FAQSection'
 
 
 export const Route = createFileRoute('/')({
