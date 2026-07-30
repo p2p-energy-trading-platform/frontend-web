8e4c64e6b2c72893cf03e269a8047809b88f1437
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-31 03:55:58 +0530

    refactor: move basic feature section component into dedicated folder

---
+++ src/components/landing/feature-section/FeaturesSection.tsx
@@ -0,0 +1,7 @@
+export default function FeatureSection() {
+  return (
+    <section className="border border-red-500 bg-background flex flex-col items-center justify-center">
+      
+    </section>
+  )
+}
