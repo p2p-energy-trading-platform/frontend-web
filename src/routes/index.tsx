d8f3b0c408573ce6fc3f30ab0887788635ed9510
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-07-31 03:47:34 +0530

    feat: add final cta section to landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import HeroSection from '#/components/landing/hero-section/HeroSection'
 import HowItWorksSection from '@/components/landing/howitworks-section/HowItWorksSection'
 import BenifitsSection from '@/components/landing/benifits-section/BenefitsSection'
 import FAQSection from '@/components/landing/faq-section/FAQSection'
+import CTASection from '@/components/landing/cta-section/CTASection'
 
 
 export const Route = createFileRoute('/')({
@@ -19,6 +20,8 @@ function Home() {
       <BenifitsSection />
 
       <FAQSection />
+
+      <CTASection />
     </main>
   )
 }
