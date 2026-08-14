c333fb94e3925de1dbcb5f47a3b33d745abb4bb9
Sivajan-Jeyaseelan <jeyaseelansivajan@gmail.com>
2026-08-14 10:14:14 +0530

    fix: change trade history files names

--- src/routes/tradehistory.tsx
+++ src/routes/tradehistory.tsx
@@ -2,14 +2,14 @@ import { createFileRoute } from '@tanstack/react-router'
 
 import PageHeader from '#/components/page-components/Header'
 import Sidebar from '#/components/page-components/Sidebar'
-import { TradesTab } from '#/components/history/History'
+import { TradesTab } from '#/components/history/TradeHistory'
 
 
-export const Route = createFileRoute('/history')({
-  component: History,
+export const Route = createFileRoute('/tradehistory')({
+  component: TradeHistory,
 })
 
-function History() {
+function TradeHistory() {
 
   const user = {
     name: 'Sara A.',
